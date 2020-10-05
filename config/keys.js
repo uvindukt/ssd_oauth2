446963e9d72d4c4ecbdb6a1b03e38a5dc81d82f1
Uvindu Sanjana <it17158046@my.sliit.lk>
2020-10-05 21:03:48 +0530

    config added

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+    google: {
+        "clientId": "391706356877-90c465mgo0pueve8t05sfqrph49nf04n.apps.googleusercontent.com",
+        "clientSecret": "xhEnwFlAvZ0NhieTNfcyChNS",
+        "redirectUris":["http://localhost:5000/auth/google/redirect"]
+    },
+    session: {
+        secret: "bdi58dm8io4f93en34mf9"
+    }
+}
